8e351e3bec2737e9a5765161d05d21d1d89531a1
prasiskajuliyanti <prasiskajuliyanti13@gmail.com>
2019-08-02 15:08:35 +0700

    update

---
+++ jambu.php
@@ -0,0 +1,7 @@
+<?php
+    $harga = 15000;
+    $kg = $harga*30;
+    $perdus = 6*2000;
+    $jumlah =$kg+$perdus;
+    echo"hasil seluruh jambu dan dus adalah : Rp.$jumlah";
+?>
---
+++ soal.php
@@ -0,0 +1,12 @@
+<?php
+
+    $tama=1;
+    $jtoni=0.5;
+    $jTiniTama=14;
+    $jTiniToni=34;
+    echo"umur tama= $jtamaX<br>";
+    echo"umur toni =$jtoni x<br>";
+    echo"$umurToni+$umuTama= $jTiniToni<br>";
+    echo"$jtoni+$jtama-$jtinitama=$jtinitoni<br>";
+
+?>
